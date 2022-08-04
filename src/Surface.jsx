beb69e8750350f754746376bbd439dcbac987705
Hussain Fazaal <fazaalmv@gmail.com>
2022-08-04 16:42:14 +0500

    Added Palette screen

--- src/Surface.jsx
+++ src/Surface.jsx
@@ -1,21 +1,21 @@
+import { useState } from 'react';
 import Navbar from './Navbar';
 
 export default function Surface() {
-  const daysTillNov4 = () => {
-    const today = new Date();
-    const nov4 = new Date(today.getFullYear(), 10, 4);
-    const diff = nov4 - today;
-    return Math.ceil(diff / (1000 * 60 * 60 * 24));
-  };
+  const [showPalette, setShowPalette] = useState(false);
 
   return (
-    <div className="absolute z-10 flex flex-col items-center justify-between min-w-full min-h-screen px-12 py-16 bg-yellow-100 bg-opacity-20 bg-blend-color backdrop-filter backdrop-blur-sm">
-      <Countdown onClick={() => console.log('clicked')} />
+    <div className="absolute z-10 flex flex-col items-center justify-between min-w-full min-h-screen px-12 py-16 bg-yellow-[C1CDC3] bg-opacity-20 bg-blend-color backdrop-filter">
+      {showPalette ? (
+        <Palette onNavigate={() => setShowPalette(false)} />
+      ) : (
+        <Countdown onNavigate={() => setShowPalette(true)} />
+      )}
     </div>
   );
 }
 
-function Countdown({ onClick }) {
+function Countdown({ onNavigate }) {
   const daysTillNov4 = () => {
     const today = new Date();
     const nov4 = new Date(today.getFullYear(), 10, 4);
@@ -23,18 +23,48 @@ function Countdown({ onClick }) {
     return Math.ceil(diff / (1000 * 60 * 60 * 24));
   };
   return (
-    <>
+    <div className="flex flex-col items-center justify-between flex-1 min-w-full py-6 ">
       <div className="flex flex-col items-center mt-12">
         <h1 className="text-9xl">{daysTillNov4()}</h1>
         <h5 className="text-6xl">Days</h5>
       </div>
       <button
         className="cursor-pointer hover:font-medium animate-pulse"
-        onClick={onClick}
+        onClick={onNavigate}
       >
         View Color Palette ⟶
       </button>
-      <div className="text-xl font-semibold">Shahu & Fazo</div>
-    </>
+      <Footer />
+    </div>
   );
 }
+
+function Palette({ onNavigate }) {
+  return (
+    <div className="flex flex-col items-center justify-between flex-1 min-w-full py-6 bg-white">
+      <div className="flex flex-col space-y-4">
+        <Color value="#99A19C" />
+        <Color value="#C1CDC3" />
+        <Color value="#B49AAC" />
+        <Color value="#D3C4CB" />
+      </div>
+      <button onClick={onNavigate}>Back</button>
+      <Footer />
+    </div>
+  );
+}
+
+const Logo = () => <div className="text-xl font-semibold">Shahu & Fazo</div>;
+
+const Color = ({ value }) => (
+  <svg height="100" width="100">
+    <circle cx="50" cy="50" r="40" fill={value} />
+  </svg>
+);
+
+const Footer = () => (
+  <div className="flex flex-col items-center space-y-2">
+    <Logo />
+    <h6 className="text-md">04/11/2022</h6>
+  </div>
+);
